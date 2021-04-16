c02c2c4e5a02d0cfc569021d8b0ab0a640352761
Matheus Rangel <matheus.rangel10@hotmail.com>
2021-04-16 16:59:17 -0300

    fix log

--- log.js
+++ log.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 function writeLog(message) {
-    let text = message + "\r\n";
-    fs.appendFile("log.log", message, function (err) {
+    let text = `${message} \r\n`;
+    fs.appendFile("log.log", text, function (err) {
         if (err) {
             return trueLog(err);
         }
